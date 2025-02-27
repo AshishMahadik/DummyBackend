9314824c484251b255c8bbb41f6191deb72f04cc
aniket <aniketvparkhi@gmail.com>
2025-02-27 18:03:47 +0530

    formatting

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const EmployeeSchema = new mongoose.Schema({
+const EmployeeSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     position: { type: String, required: true },
     department: { type: String },
     salary: { type: Number, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" } // To track who created the employee
-}, { timestamps: true });
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // To track who created the employee
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model("Employee", EmployeeSchema);
+module.exports = mongoose.model('Employee', EmployeeSchema);
